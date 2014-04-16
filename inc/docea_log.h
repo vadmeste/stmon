a6bb519045e432661808f3d9bfdf73a7d4c74594
Anis ELLEUCH <anis.elleuch@gmail.com>
2014-04-16 16:53:20 +0100

    Move all logging code to docea_log .c/.h

---
+++ inc/docea_log.h
@@ -0,0 +1,38 @@
+
+#ifndef __DOCEA_LOG_H_
+#define __DOCEA_LOG_H_
+
+
+#include "stm32f4xx_rcc.h" 
+#include "stm32f4xx_rtc.h" 
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "cbuf.h"
+#include "rtc.h"
+
+typedef struct 
+{
+      uint8_t tab[32];
+} Table_TypeDef;
+
+
+CircularBuffer  g_LogCB;
+uint8_t g_LogCB_enable;
+
+short st_subsecond;
+
+
+void msg(char* buf);
+
+void do_log_append(char *fmt, uint8_t show_timestamp, va_list va);
+void log_append(char *fmt, ...);
+void dirty_log_append(char* fmt, ...);
+void log_raw_append(char* fmt, ...);
+void dirty_log_raw_append(char* fmt, ...);
+void clockupdate_log(RCC_BUS_TYPE bus, uint32_t periph, FunctionalState newstate);
+void print_log();
+
+
+#endif
